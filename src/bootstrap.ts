0d2b354610a4cb29cbafba0449f83274c251711b
kewos554321 <410413042@gms.ndhu.edu.tw>
2025-10-13 20:18:05 +0800

    fix: update global API prefix from 'api' to 'app-api' for improved clarity and consistency in routing

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export async function createApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api', { exclude: ['/', 'health'] });
+  app.setGlobalPrefix('app-api', { exclude: ['/', 'health'] });
   app.enableCors({
     origin: true,
     credentials: true,
